ecff40858327ea97ca4efc45f04d35297abce99f
0xbirch <0xbirch@protonmail.com>
2021-12-22 12:01:34 -0500

    removing tmp file

--- src/hooks/contracts/tmp.js
+++
@@ -1,4 +0,0 @@
-// // Use this code when you go to call the evaluate function in the contract
-// const abiInterface = new ethers.utils.Interface(Commitment.abi)
-// const contract = new Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", abiInterface)
-// const { state, send } = useContractFunction(contract, "evaluate")
