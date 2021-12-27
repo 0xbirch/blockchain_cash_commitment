9ad95a08a531d701b6f0174b27b31c62f0283fc3
0xbirch <0xbirch@protonmail.com>
2021-12-27 11:50:51 -0500

    adding more deploy functionality to form

--- src/components/CommitmentForm.tsx
+++ src/components/CommitmentForm.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Alert, AlertIcon, AlertTitle, AlertDescription, CloseButton, Box, Text, FormControl, Select, FormLabel, FormErrorMessage, Button, InputGroup, InputRightAddon, Stack, NumberInput, NumberInputField, RadioGroup, Radio } from "@chakra-ui/react";
 import DatePicker from "react-datepicker"
+import { useContractFunction, useEtherBalance, TransactionState, getStoredTransactionState } from "@usedapp/core"
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { useContractFunction, useEtherBalance, TransactionState, getStoredTransactionState } from "@usedapp/core"
 import { formatEther } from '@ethersproject/units'
 import { ethers } from "ethers"
 import Commitment from "../abi/Commitment.json"
@@ -14,18 +14,33 @@ export default function CommitementForm(props: any) {
     const { register, setValue, handleSubmit, formState: { errors } } = useForm();
     const [startDate, setStartDate] = useState(new Date())
     const [recipient, setRecipient] = useState("1")
-    let etherBalance = useEtherBalance(props.account)
+	let etherBalance = useEtherBalance(props.account)
 
-    const deployContract = async (data: any) => {
-	//	const newContract = new ethers.ContractFactory(Commitment.abi, Commitment.bytecode)
-		console.log(data)
-	//	newContract.deploy(data.goal, data.)
-    }
-	
+    const deployContract = async (goal: number, deadline: number, beneficiary: string | undefined, amountStaked: number, amountToSave: number) => {
+		console.log(goal, deadline, beneficiary, amountStaked, amountToSave)
+		const newContract = new ethers.ContractFactory(Commitment.abi, Commitment.bytecode)
+		debugger
+		try {
+			await newContract.deploy(goal, deadline, beneficiary, amountToSave, {value: amountStaked})
+		} catch(error) {
+			console.error(error)
+		}
+	}	
 
     let alert = <h1>This is where an alert would go</h1>
     function onSubmit(data: any) {
-        deployContract(data)
+		console.log("data", data)	
+		const beneficiaryMapping = new Map<string, string>([
+			["0", "0x70997970c51812dc3a010c7d01b50e0d17dc79c8"],
+			["1", "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc"],
+			["2", "0x90f79bf6eb2c4f870365e785982e1f101e93b906"],
+			["3", "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65"],
+			["4", "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc"]
+		])
+
+		data.amountStaked = ethers.utils.parseEther(data.amountStaked)
+		data.amountSaved = ethers.utils.parseEther(data.amountSaved)
+        deployContract(data.goal, data.deadline.getTime(), beneficiaryMapping.get(data.recipient), data.amountStaked, data.amountSaved)
         alert = (
             <Alert status="success">
                 <AlertIcon />
@@ -34,6 +49,24 @@ export default function CommitementForm(props: any) {
             </Alert>
         )
     }
+
+	const amountToSaveComponent = (
+		<div>
+			<FormLabel pt={10} htmlFor="amountSaved" color="white">How much would you like to save?</FormLabel>
+			<InputGroup>
+				<NumberInput defaultValue={1.0} precision={10} max={10000} min={0.0000000001}>
+					<NumberInputField
+						color="white"
+						id="amountSaved"
+						placeholder="3"
+						{...register("amountSaved", {
+							required: "This is required"
+						})} />
+				</NumberInput>
+				<InputRightAddon children="ETH" />
+			</InputGroup>
+		</div>
+	)
 
     return (
         <div>
@@ -53,6 +86,7 @@ export default function CommitementForm(props: any) {
                             required: "This is required"
                         })} value='0'>Save or Make Money</option>
                     </Select>
+					{true && amountToSaveComponent}
                     <FormLabel pt={10} htmlFor="timeframe" color="white">When do you want to accomplish your goal by?</FormLabel>
                             <DatePicker {...register('deadline', { value: startDate})} selected={startDate} onChange={(date: any) => {
 								setValue("deadline", date)
@@ -74,8 +108,8 @@ export default function CommitementForm(props: any) {
                     <FormLabel pt={10} htmlFor="recipient" color="white">Who do you want your stake to go to if you don't meet your goal?</FormLabel>
                     <RadioGroup onChange={setRecipient} value={recipient}>
                         <Stack>
-                            <Radio {...register("recipient")} value="1" color="white">Trump</Radio>
-                            <Radio {...register("recipient")} value="2" color="white">Biden</Radio>
+                            <Radio {...register("recipient")} value="0" color="white">Trump</Radio>
+                            <Radio {...register("recipient")} value="1" color="white">Biden</Radio>
                         </Stack>
                     </RadioGroup>
                     <FormErrorMessage>
@@ -90,3 +124,4 @@ export default function CommitementForm(props: any) {
         </div>
     )
 }
+	
