6d254ca7b5eca213ce0ea41701da7721761161ad
0xbirch <0xbirch@protonmail.com>
2022-01-09 11:19:33 -0500

    adding more tests to check up keep and implemented perform upkeep now need to test it

--- packages/smart_contract/contracts/CommitmentCore.sol
+++ packages/smart_contract/contracts/CommitmentCore.sol
@@ -5,6 +5,7 @@ import "./Commitment.sol";
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "hardhat/console.sol";
 
 contract CommitmentCore is KeeperCompatibleInterface {
 	using EnumerableMap for EnumerableMap.UintToAddressMap;
@@ -28,29 +29,45 @@ contract CommitmentCore is KeeperCompatibleInterface {
 	
 	function checkUpkeep(bytes calldata checkdata) external override view returns (bool, bytes memory) {
 		bool upKeep = false;
-		address[] memory payableAddresses;
+		address[50] memory payableAddresses;
+		uint[50] memory amountsStaked;
 		uint ownerFee;
+		uint8 count = 0;
 		for (uint i = 0; i < eoaToContractAddressCommitments.length(); i++) {
 			(uint256 committer, address contractAddress) = eoaToContractAddressCommitments.at(i);			
 			Commitment commitment = Commitment(contractAddress);
 		    if (commitment.isTimeToEvaluate()) {
-				upKeep = true;
 				if (commitment.didAccomplishGoal()) {
-				//	payableAddresses.push(committer);
+					payableAddresses[count] = address(uint160(committer));
+					amountsStaked[count] = contractAddress.balance;
 				} else {
-				//	payableAddresses.push(commitment.getRecipient());
-					ownerFee.add(commitment.calculateOwnerFee());
+					payableAddresses[count] = commitment.getRecipient();
+					(uint recipientAmount, uint ownerAmount) = calculateOwnerSplit(contractAddress);
+					amountsStaked[count] = recipientAmount;
+					ownerFee = ownerFee.add(ownerAmount);
 			}
-				
+			upKeep = true;
+			++count;	
 		}
 				
 		}			
-		return (upKeep, abi.encode(payableAddresses, ownerFee));
+		return (upKeep, abi.encode(payableAddresses, amountsStaked, ownerFee));
 	}
 
 
 	function performUpkeep(bytes calldata performData) external override {
-			
+		(address[50] memory payableAddresses, uint[50] memory amountsStaked, uint ownerFee) = abi.decode(performData, (address[50], uint[50], uint));	
+		for (uint8 i = 0; i < payableAddresses.length; i++) {
+			if (payableAddresses[i] == address(0)) {
+				break;
+			}	
+			console.log(amountsStaked[i]);
+			// This is failing when it goes to estimate the gas. is there something wrong with the wei thats being passed in?
+      		payable(payableAddresses[i]).transfer(amountsStaked[i]); 
+		}
+		if (ownerFee > 0) {
+			payable(0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199).transfer(ownerFee);
+		}
 	}
 	
 	function getCommitment(address committer) external view returns (address) {
@@ -60,6 +77,12 @@ contract CommitmentCore is KeeperCompatibleInterface {
 	function convertToUint(address addr) private pure returns (uint) {
 		return uint256(uint160(addr));	
 	}
+
+	 function calculateOwnerSplit(address contractAddress) private view returns (uint, uint) {
+		uint balance = contractAddress.balance;
+		uint ownerAmount = balance.mul(375).div(10000);
+		return (balance.sub(ownerAmount), ownerAmount);
+	 }
 
 	receive() external payable {}
 }
--- packages/smart_contract/test/commitmentCore-test.js
+++ packages/smart_contract/test/commitmentCore-test.js
@@ -44,7 +44,7 @@ describe("CommittmentCore tests", function () {
 		assert(events[0].args[1] === await commitmentContract.getCommitment("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
 	})	
 
-	it.only("Should validate that check up keep should return false if there are no commitments to execute", async () => {
+	it("Should validate that check up keep should return false if there are no commitments to execute", async () => {
 		const date = new Date()
         const goalDate = Math.round(date.setDate(date.getDate() + 1) / 1000)
 		await commitmentContract.newCommitment(0, 
@@ -54,7 +54,7 @@ describe("CommittmentCore tests", function () {
 			{value: ethers.utils.parseEther("2")})
 		const data = "0x00";
 		const returns = await commitmentContract.checkUpkeep(data)
-		assert.deepEqual(returns, [false, '0x'])
+		assert(returns[0] === false)
 	})	
 	
 	it("Should validate that check up keep should return true if there are commitments to execute", async () => {
@@ -67,7 +67,92 @@ describe("CommittmentCore tests", function () {
 			{value: ethers.utils.parseEther("2")})
 		const data = "0x00";
 		const returns = await commitmentContract.checkUpkeep(data)
-		assert.deepEqual(returns, [true, '0x'])
+		assert(returns[0] === true)
 	})	
+	
+	it("Should validate that check up keep should return the correct address list if there are commitments to execute", async () => {
+		const date = new Date()
+        const goalDate = Math.round(date.setDate(date.getDate() - 1) / 1000)
+		await commitmentContract.newCommitment(0, 
+			goalDate,
+			"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", 
+			ethers.utils.parseEther("1"), 
+			{value: ethers.utils.parseEther("2")})
+		const data = "0x00";
+		const returns = await commitmentContract.checkUpkeep(data)
+		assert(returns[1].includes("f39fd6e51aad88f6f4ce6ab8827279cfffb92266")) 
+		assert(returns[1].includes("1bc16d674ec80000"))
+
+	})	
+	
+	it("Should validate that check up keep should return the correct address list when there is more than one address", async () => {
+		// This should only pass if the did accomplish method is returning true; 
+		const date = new Date()
+        const goalDate = Math.round(date.setDate(date.getDate() - 1) / 1000)
+		await commitmentContract.newCommitment(0, 
+			goalDate,
+			"0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc", 
+			ethers.utils.parseEther("1"), 
+			{value: ethers.utils.parseEther("2")})
+		const newSigner = commitmentContract.provider.getSigner("0x70997970c51812dc3a010c7d01b50e0d17dc79c8")
+		const newContract = commitmentContract.connect(newSigner)
+        const goalDate2 = Math.round(date.setDate(date.getDate() - 1) / 1000)
+		await newContract.newCommitment(0, 
+			goalDate2,
+			"0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc", 
+			ethers.utils.parseEther("1"), 
+			{value: ethers.utils.parseEther("1")})
+		const data2 = "0x00";
+		const returns = await commitmentContract.checkUpkeep(data2)
+		assert(returns[1].includes("f39fd6e51aad88f6f4ce6ab8827279cfffb92266")) 
+		assert(returns[1].includes("1bc16d674ec80000"))
+		assert(returns[1].includes("70997970c51812dc3a010c7d01b50e0d17dc79c8")) 
+		assert(returns[1].includes("de0b6b3a76400000"))
+
+	})	
+	
+	it("Should validate that checkUpkeep returns the recipent and cut if someone diddnt accomplish their goal", async () => {
+		// This should only pass if the did accomplish method is returning false; 
+		const date = new Date()
+        const goalDate = Math.round(date.setDate(date.getDate() - 1) / 1000)
+		await commitmentContract.newCommitment(0, 
+			goalDate,
+			"0x70997970c51812dc3a010c7d01b50e0d17dc79c8", 
+			ethers.utils.parseEther("1"), 
+			{value: ethers.utils.parseEther("2")})
+		const data = "0x00";
+		const returns = await commitmentContract.checkUpkeep(data)
+
+		const balance = ethers.utils.parseEther("2")  
+		console.log("balance", balance)
+		const cut = balance.mul(375).div(10000)
+		const expectedBytes = ethers.utils.defaultAbiCoder.encode(["uint", "uint"],[balance.sub(cut).toString(), cut.toString()])
+	 	assert(returns[1].includes("70997970c51812dc3a010c7d01b50e0d17dc79c8"))
+		assert(returns[1].includes("10a741a46278000"))
+		assert(returns[1].includes("1ab6f94d08a08000"))
+
+	})	
+
+	it.only("performUpkeep should pay the address that needs to be paid the right amount no fee", async () => {
+		const date = new Date()
+        const goalDate = Math.round(date.setDate(date.getDate() - 1) / 1000)
+		await commitmentContract.newCommitment(0, 
+			goalDate,
+			"0x70997970c51812dc3a010c7d01b50e0d17dc79c8", 
+			ethers.utils.parseEther("1"), 
+			{value: ethers.utils.parseEther("2")})
+		const beginningBalance = ethers.getDefaultProvider().getBalance("0x70997970c51812dc3a010c7d01b50e0d17dc79c8")
+		const data = "0x00";
+		const returns = await commitmentContract.checkUpkeep(data)
+//		console.log(returns[1])
+		await commitmentContract.performUpkeep(returns[1]);	
+		const endingBalance = ethers.getDefaultProvider().getBalance("0x70997970c51812dc3a010c7d01b50e0d17dc79c8")
+		assert(endingBalance.sub(beginningBalance) === ethers.utils.parseEther("2"))
+	})	
+	it("performUpkeep should pay the address that needs to be paid the right amount with fee", async () => {
+	})	
+	it("performUpkeep should pay the addresses that needs to be paid the right amount with fee", async () => {
+	})	
+
 
 })
